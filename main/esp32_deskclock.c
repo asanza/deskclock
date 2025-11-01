58fd790718dcf269abf0d6bd3edcc45914d67c2c
Diego Asanza <f.asanza@gmail.com>
2025-11-01 12:44:08 +0100

    f
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- main/esp32_deskclock.c
+++ main/esp32_deskclock.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <epd_driver.h>
 
 void app_main(void)
 {
-
+    Rect_t area = {
+        .x = 230,
+        .y = 0,
+        .width = 20,
+        .height = 20,
+    };
+    epd_poweron();
+    epd_clear_area(area);
 }
