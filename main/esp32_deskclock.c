01a1e0d364aa0ab6bbf7de52d1acd5c4c0bd401d
Diego Asanza <f.asanza@gmail.com>
2025-11-08 20:25:29 +0100

    f
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- main/esp32_deskclock.c
+++ main/esp32_deskclock.c
@@ -153,41 +153,41 @@ read_battery_voltage(void)
     // Battery max ~4.2V, ADC reads ~2.1V max
     // ADC reference is 3.3V for ESP32-S3
     
-    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_handle_t adc_handle;
     adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_UNIT_1,
+        .unit_id = ADC_UNIT_2,
         .ulp_mode = ADC_ULP_MODE_DISABLE,
     };
-    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc1_handle));
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
 
     adc_oneshot_chan_cfg_t config = {
         .bitwidth = ADC_BITWIDTH_DEFAULT,
         .atten = ADC_ATTEN_DB_12,  // Full range: 0-3.3V
     };
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_3, &config));
 
     // Read ADC value
     int adc_raw;
-    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_3, &adc_raw));
+    ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL_3, &adc_raw));
     
     // Initialize calibration
-    adc_cali_handle_t adc1_cali_handle = NULL;
+    adc_cali_handle_t adc_cali_handle = NULL;
     adc_cali_curve_fitting_config_t cali_config = {
-        .unit_id = ADC_UNIT_1,
+        .unit_id = ADC_UNIT_2,
         .atten = ADC_ATTEN_DB_12,
         .bitwidth = ADC_BITWIDTH_DEFAULT,
     };
     
-    esp_err_t ret = adc_cali_create_scheme_curve_fitting(&cali_config, &adc1_cali_handle);
+    esp_err_t ret = adc_cali_create_scheme_curve_fitting(&cali_config, &adc_cali_handle);
     
     int voltage_mv = 0;
     if (ret == ESP_OK) {
-        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw, &voltage_mv));
-        adc_cali_delete_scheme_curve_fitting(adc1_cali_handle);
+        ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc_cali_handle, adc_raw, &voltage_mv));
+        adc_cali_delete_scheme_curve_fitting(adc_cali_handle);
     }
     
     // Clean up
-    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc_handle));
     
     // Convert to battery voltage (multiply by 2 for voltage divider)
     float battery_voltage = (voltage_mv * 2.0f) / 1000.0f;
